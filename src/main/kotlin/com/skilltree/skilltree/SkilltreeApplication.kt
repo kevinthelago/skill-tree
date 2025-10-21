e1ea725cd2ad47073cc3a48eefc506c01fa7e847
Kevin Lago <kevinthelago@gmail.com>
2025-10-21 15:49:48 -0400

    Initial Commit

---
+++ src/main/kotlin/com/skilltree/skilltree/SkilltreeApplication.kt
@@ -0,0 +1,11 @@
+package com.skilltree.skilltree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkilltreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkilltreeApplication>(*args)
+}
